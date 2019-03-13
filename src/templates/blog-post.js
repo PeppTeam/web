691b280feedf5abb91de38d0205100de0b4a2f35
Emma Klint <>
2019-03-13 08:00:22 +0100

    Blog post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,49 +4,65 @@ import styled from "styled-components";
 import Img from "gatsby-image";
 import moment from "moment";
 import { Wide, Section, Narrow } from "../components/Layout";
-import { H1, Intro, Content } from "../components/Typography";
+import { H1, P,Intro, Content } from "../components/Typography";
+import { Hero } from "../components/Layout/Hero";
 
-const Image = styled(Img)`
-  width: auto;
-  height: 600px;
-  margin: 50px;
+const Image  = styled(Img)`
+width: auto
+margin-bottom: 10rem;
+
+`
+
+const AuthorImage = styled(Img)`
+  width: 100px;
+  height: 100px;
+  background-size: cover;
+  display: block;
+  border-radius: 50%;
+  -webkit-border-radius: 50%;
+  -moz-border-radius: 50%;
+  margin-bottom: 1rem;
 `;
 
 export default function BlogPost({ data }) {
   const post = data.contentfulBlogPost;
   return (
     <Page>
-      <Section>
-        <Narrow>
+      <Hero>
+        <Narrow mb={4}>
           <H1>{post.title}</H1>
-          <div>
-            {post.tags.map(tag => {
+          <Intro>
+            Tags: {post.tags.map(tag => {
               return <p key={tag}>{tag}</p>;
             })}
-          </div>
-
-          <p> {moment(post.publishDate).format("L")}</p>
-        </Narrow>
-        <Wide>
-          <Image fluid={post.heroImage.fluid} />
-        </Wide>
-        <Narrow>
+          </Intro>
+          <Intro> {moment(post.publishDate).format("L")}</Intro>
           <Intro
             dangerouslySetInnerHTML={{
               __html: post.description.childMarkdownRemark.html
             }}
           />
+        </Narrow>
+      </Hero>
+      <Section>
+        <Wide>
+          <Image
+            fluid={post.heroImage.fluid}
+          />
+        </Wide>
+        </Section>
+        <Section>
+        <Narrow>
           <Content
             dangerouslySetInnerHTML={{
               __html: post.body.childMarkdownRemark.html
             }}
           />
-          <div>
-            <Image fluid={post.author.image.fluid} />
-
-            <h2>{post.author.name}</h2>
-          </div>
         </Narrow>
+      </Section>
+      <Section>
+      <AuthorImage fluid={post.author.image.fluid} />
+        <P>{post.author.name}</P>
       </Section>
     </Page>
   );
